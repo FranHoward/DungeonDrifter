c6f7837328ad2592451d0c41b884843f4f97d8d5
FranHoward <hej986436@gmail.com>
2026-07-07 22:14:51 +0800

    attack

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField] private float range = 1.5f;
+    [SerializeField] private LayerMask enemyLayer;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) DoAttack();
+    }
+
+    private void DoAttack()
+    {
+        Vector3 center = transform.position + transform.forward * range * 0.5f;
+        Collider[] hits = Physics.OverlapSphere(center, range, enemyLayer);
+
+        foreach(var hit in hits)
+        {
+            Debug.Log($"命中: {hit.name}");
+            if (hit.TryGetComponent<Renderer>(out var r))
+                r.material.color = Color.red;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position + transform.forward * range * 0.5f, range);
+    }
+}
